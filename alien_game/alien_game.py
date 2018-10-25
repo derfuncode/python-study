c8e82ed6cecd27e408634b6bdf023d4799a6a541
derfuncode <caodefang@139.com>
2018-10-25 17:18:17 +0800

    2018年10月25日17:18:15

--- alien_game/alien_game.py
+++ alien_game/alien_game.py
@@ -15,6 +15,9 @@ def run_game():
 
     bullets = Group()
 
+    aliens = Group()
+    gf.create_fleet(ai_settings,screen,aliens)
+
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
@@ -23,6 +26,6 @@ def run_game():
         #print(len(bullets))
     
 
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
--- alien_game/game_functions.py
+++ alien_game/game_functions.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from bullet import Bullet
+from alien import Alien
 
 def check_keydown_events(event,ai_settings,screen,ship,bullets):
     if event.key == pygame.K_RIGHT:
@@ -10,6 +11,8 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings,screen,ship,bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 def check_keyup_events(event,ship):
     if event.key == pygame.K_RIGHT:
@@ -26,12 +29,13 @@ def check_events(ai_settings,screen,ship,bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
-def update_screen(ai_settings,screen,ship,bullets):
+def update_screen(ai_settings,screen,ship,aliens,bullets):
     screen.fill(ai_settings.bg_color)
 
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    aliens.draw(screen)
 
     pygame.display.flip()
 
@@ -45,3 +49,24 @@ def fire_bullet(ai_settings,screen,ship,bullets):
     if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings,screen,ship)
         bullets.add(new_bullet)
+
+def get_number_aliens_x(ai_settings,alien_width):
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2*alien_width))
+    return number_aliens_x
+
+def create_alien(ai_settings,screen,aliens,alien_number):
+    alien = Alien(ai_settings,screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2*alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
+def create_fleet(ai_settings,screen,aliens):
+    alien = Alien(ai_settings,screen)
+    number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
+
+    for alien_number in range(number_aliens_x):
+        create_alien(ai_settings,screen,aliens,alien_number)
+        
+    
