2caa190365037d644f74257a7c02e3b344467551
derfuncode <caodefang@139.com>
2018-10-29 12:59:30 +0800

    20181029

--- alien_game/alien_game.py
+++ alien_game/alien_game.py
@@ -8,7 +8,7 @@ import game_functions as gf
 def run_game():
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_hight))
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("外星人入侵")
 
     ship = Ship(ai_settings,screen)
@@ -16,16 +16,13 @@ def run_game():
     bullets = Group()
 
     aliens = Group()
-    gf.create_fleet(ai_settings,screen,aliens)
+    gf.create_fleet(ai_settings,screen,ship,aliens)
 
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_bullets(bullets)
-
-        #print(len(bullets))
-    
-
+        gf.update_bullets(ai_settings,screen,ship,aliens,bullets) 
+        gf.update_aliens(ai_settings,ship,aliens)
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
--- alien_game/settings.py
+++ alien_game/settings.py
@@ -1,15 +1,18 @@
 class Settings():
     def __init__(self):
         self.screen_width = 1200
-        self.screen_hight = 800
+        self.screen_height = 800
         self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 50
+        self.fleet_direction = 1
 
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_speed_factor = 5
+        self.bullet_width = 10
         self.bullet_height  = 15
         self.bullet_color = 60,60,60
-        self.bullets_allowed = 3
+        self.bullets_allowed = 10
 
 
 
